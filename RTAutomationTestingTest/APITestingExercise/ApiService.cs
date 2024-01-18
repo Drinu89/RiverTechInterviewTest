9bc223b5103f3c9aa4ff3ba8b888df2d03db4088
Andre Coleiro <andrecoleiro@gmail.com>
2024-01-18 08:13:50 +0100

    Added the structure of the GET response as classes so that I can Assert each field and value.

--- RTAutomationTestingTest/APITestingExercise/ApiService.cs
+++ RTAutomationTestingTest/APITestingExercise/ApiService.cs
@@ -3,7 +3,6 @@ namespace RTAutomationTestingTest.APITestingExercise;
 public interface IApiService
 {
     HttpResponseMessage SendAGetHTTPRequest(string baseUrl);
-    List<string> GetFields();
 }
 
 public class ApiService : IApiService
@@ -11,6 +10,40 @@ public class ApiService : IApiService
     public class ApiBuilder()
     {
         public string ApiUrl { get; init; }
+    }
+    
+    public class Address
+    {
+        public string street { get; set; }
+        public string suite { get; set; }
+        public string city { get; set; }
+        public string zipcode { get; set; }
+        public Geo geo { get; set; }
+    }
+
+    public class Company
+    {
+        public string name { get; set; }
+        public string catchPhrase { get; set; }
+        public string bs { get; set; }
+    }
+
+    public class Geo
+    {
+        public string lat { get; set; }
+        public string lng { get; set; }
+    }
+
+    public class ApiResponse
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string username { get; set; }
+        public string email { get; set; }
+        public Address address { get; set; }
+        public string phone { get; set; }
+        public string website { get; set; }
+        public Company company { get; set; }
     }
 
     public HttpResponseMessage SendAGetHTTPRequest(string baseUrl)
@@ -23,10 +56,5 @@ public class ApiService : IApiService
 
     }
 
-    public List<string> GetFields()
-    {
-        List<string> fieldList = new List<string> { "id", "name", "username" };
 
-        return fieldList;
-    }
 }
